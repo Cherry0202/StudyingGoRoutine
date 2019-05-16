9c7d8cbf34d2b306f9c5004b4d6ff1333355acbc
Cherry0202 <ex00ia2432@gmail.com>
2019-05-16 11:12:57 +0900

    [done]チャネルの冗長化を１つに

--- Gorutine.go
+++ Gorutine.go
@@ -9,16 +9,14 @@ func main() {
 	log.Print("started")
 
 //	チャネル
-	sleep1_finished := make(chan bool)
-	sleep2_finished := make(chan bool)
-	sleep3_finished := make(chan bool)
+	finished := make(chan bool)
 
 	go func() {
 		// 1秒かかるコマンド
 		log.Print("sleep1 started.")
 		time.Sleep(1 * time.Second)
 		log.Print("sleep1 finished.")
-		sleep1_finished<-true
+		finished<-true
 	}()
 
 	go func() {
@@ -26,7 +24,7 @@ func main() {
 		log.Print("sleep2 started.")
 		time.Sleep(2 * time.Second)
 		log.Print("sleep1 finished.")
-		sleep2_finished<-true
+		finished<-true
 	}()
 
 	go func() {
@@ -34,15 +32,16 @@ func main() {
 		log.Print("sleep3 started.")
 		time.Sleep(3 * time.Second)
 		log.Print("sleep1 finished.")
-		sleep3_finished<-true
+		finished<-true
 	}()
 
 
 
 	//終わるまで待つ
-	<- sleep1_finished
-	<- sleep2_finished
-	<- sleep3_finished
+
+	for i := 1; i<=3; i++ {
+		<- finished
+	}
 
 	log.Print("finished")
 
